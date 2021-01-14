a257b7f48f277910ea134aa73ddc6ac28d2897f7
Jesse Brown <brownjesse8895@yahoo.com>
2021-01-14 22:13:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,14 +6,15 @@ end
 puts addition(4, 5)
 
 def subtraction(num1, num2)
-sum = num1 - num2 
-sum 
+subtraction = num1 - num2 
+subtraction 
 end
 puts subtraction(10,5)
 def division(num1, num2)
-sum 
+division = num1 / num2 
+division
 end
-
+puts division(50,2 )
 def multiplication(num1, num2)
 
 end
